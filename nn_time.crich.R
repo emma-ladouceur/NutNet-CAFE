6751abde915951ee49d2704ce5ff74c7c081ec8c
Emma Ladouceur <emma.ladouceur@idiv.de>
2019-05-28 09:25:34 +0200

    crich script for cluster

---
+++ nn_time.crich.R
@@ -0,0 +1,24 @@
+
+library(tidyverse)
+library(brms)
+
+
+path <- '/gpfs1/data/idiv_chase/emmala/NutNet'
+p.all <- read.csv(paste0(path, '/progressive_time_only.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
+
+
+p.all$site_code<-as.factor(p.all$site_code)
+p.all$site.year.id<-as.factor(p.all$site.year.id)
+p.all$block<-as.factor(p.all$block)
+p.all$plot<-as.factor(p.all$plot)
+
+p.dat2$c.rich.log<-log1p(p.dat2$c.rich)
+
+p.c.rich.i <- brm(c.rich.log ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+                  data = p.all,cores = 4, chains = 4)
+
+save(p.c.rich.i,
+     file=Sys.getenv('OFILE'))
+
+
+
---
+++ nn_time.crich.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#$ -N nn_time_p.crich
+#$ -S /bin/bash
+#$ -l h_rt=620:00:00
+#$ -l h_vmem=8G
+#$ -pe smp 4
+
+#$ -o /home/$USER/$JOB_NAME-$JOB_ID.out
+#$ -j y
+
+module load R/3.4.3-1
+export OFILE=/work/$USER/NutNet/$JOB_NAME-$JOB_ID.Rdata
+export LANG=en_US.UTF-8
+Rscript --vanilla /home/ladouceu/projects/NutNet/nn_time.schange.R
+
+
