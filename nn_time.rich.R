4da3ef31fbf173855e40af9cc46121574ebe1b3d
Emma Ladouceur <emma.ladouceur@idiv.de>
2019-12-19 07:56:04 -0500

    log rich model

--- nn_time.rich.R
+++ nn_time.rich.R
@@ -10,14 +10,16 @@ plot <- read.csv(paste0(path, '/plot_calc.csv'), header=T,fill=TRUE,sep=",",na.s
 plot$site_code<-as.factor(plot$site_code)
 plot$block<-as.factor(plot$block)
 plot$plot<-as.factor(plot$plot)
+plot$log.rich<-log(plot$rich)
 
 
-
-plot.rich.im <- brm(rich ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
+# plot.rich.im <- brm(rich ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
+#                     data = plot, cores = 4, chains = 4)
+# log
+plot.rich.log <- brm(rich ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
                     data = plot, cores = 4, chains = 4)
 
-
-save(plot.rich.im,
+save(plot.rich.log,
      file=Sys.getenv('OFILE'))
 
 
