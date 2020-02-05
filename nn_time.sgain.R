1550a639d4f5ad34876c59db78d64fe3d1204d48
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-02-05 19:03:06 +0100

    student try two

--- nn_time.sgain.R
+++ nn_time.sgain.R
@@ -21,7 +21,7 @@ p.all$plot<-as.factor(p.all$plot)
 #                 data = p.all, cores = 4, chains = 4)
 
 s.gain.s.t <- brm(s.gain ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
-                data = p.all, family=student(), cores = 4, iter = 6000 , chains = 4)
+                data = p.all, family=student(), cores = 4, iter = 6000 ,warmup = 1000, chains = 4)
 
 save(s.gain.s.t,
      file=Sys.getenv('OFILE'))
--- nn_time.sloss.R
+++ nn_time.sloss.R
@@ -6,7 +6,6 @@ library(brms)
 path <- '/gpfs1/data/idiv_chase/emmala/NutNet'
 p.all <- read.csv(paste0(path, '/cumulative_time_only2.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 
-
 p.all$site_code<-as.factor(p.all$site_code)
 p.all$site.year.id<-as.factor(p.all$site.year.id)
 p.all$block<-as.factor(p.all$block)
@@ -19,10 +18,10 @@ p.all$plot<-as.factor(p.all$plot)
 # s.loss.i <- brm(s.loss.p ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
 #                 data = p.all, cores = 4, chains = 4)
 
- s.loss.p.i <- brm(s.loss.p ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
-                 data = p.all,family=hurdle_poisson(link = "identity"), cores = 4, iter = 6000, chains = 4)
+ s.loss.s.t<- brm(s.loss.p ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+                 data = p.all,family=student(), cores = 4, iter = 6000, warmup = 1000,chains = 4)
 
-save(s.loss.p.i,
+save(s.loss.s.t,
      file=Sys.getenv('OFILE'))
 
 
