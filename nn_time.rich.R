cc75415a57a142a0fa316354d494f3347db92f23
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-10-22 14:22:23 +0200

    rich bm 5

--- nn_time.rich.R
+++ nn_time.rich.R
@@ -12,14 +12,14 @@ plot$block<-as.factor(plot$block)
 plot$plot<-as.factor(plot$plot)
 plot$log.rich<-log(plot$all.div)
 
- plot <- plot %>% group_by(site_code) %>% filter(max.year >= 3) %>%
+ plot <- plot %>% group_by(site_code) %>% filter(max.year >= 5) %>%
  ungroup()
 
 
-plot.rich.3<- brm(all.div ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
+plot.rich.5<- brm(all.div ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
                     data = plot,cores = 4,iter=6000, warmup = 1000, chains = 4)
 
-save(plot.rich.3,
+save(plot.rich.5,
      file=Sys.getenv('OFILE'))
 
 
