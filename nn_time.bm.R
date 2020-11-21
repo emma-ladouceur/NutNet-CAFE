371b024889fd9680e01e8bb815f3a62c336657c8
Emma Ladouceur <emma.ladouceur@idiv.de>
2020-11-21 14:26:44 +0100

    5 tx

--- nn_time.bm.R
+++ nn_time.bm.R
@@ -11,13 +11,13 @@ plot$site_code<-as.factor(plot$site_code)
 plot$block<-as.factor(plot$block)
 plot$plot<-as.factor(plot$plot)
 
-plot <- plot %>% group_by(site_code) %>% filter(max.year >= 3) %>%
+plot <- plot %>% group_by(site_code) %>% filter(max.year >= 5) %>%
   ungroup()
 
-plot.bm.3 <- brm(plot.mass ~ trt * year_trt + (trt * year_trt | site_code/block/plot), 
+plot.bm.5 <- brm(plot.mass ~ trt * year_trt + (trt * year_trt | site_code/block/plot), 
                   data = plot , family=student(),  cores = 4,iter=6000, warmup = 1000, chains = 4)
 
 
-save(plot.bm.3,
+save(plot.bm.5,
      file=Sys.getenv('OFILE'))
 
