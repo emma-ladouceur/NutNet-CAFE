c555ce6b540ce17c0d6597da2f244512f2bbd5d0
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-02-18 07:30:42 +0100

    study-level figure

--- study_posterior_price.R
+++ study_posterior_price.R
@@ -166,3 +166,159 @@ study.price.cloud.ctl
 
 (study.price.cloud | study.price.cloud.ctl )
 
+
+
+colnames(all.effs)
+all.effs$Quadrant<-as.factor(as.character(all.effs$Quadrant))
+levels(all.effs$Quadrant)
+
+npk.effs <- all.effs %>% select(site_code,sg.trt.rate.p, sl.trt.rate.p,cde.trt.rate.p,sloss.trt.rate.p,sgain.trt.rate.p, Quadrant) %>%
+ # gather(model ,effect, sg.trt.rate.p:sgain.trt.rate.p) %>%
+  filter(!Quadrant == "-biomass +rich")
+
+head(npk.effs)
+npk.effs$Quadrant<-factor(npk.effs$Quadrant,  levels=c("+biomass +rich",  "-biomass -rich" , "+biomass -rich"))
+  
+  
+  
+sloss.se<-ggplot() + 
+ # facet_grid(~model)+
+  geom_point(data =npk.effs, aes(y = Quadrant, x = sloss.trt.rate.p),
+             colour= "#B40F20",size = 2, position = position_jitter(height = 0.15 )) +
+  # geom_errorbar(data = rich.p, aes(x = response, ymin = eff_lower,
+  #                                  ymax = eff_upper, color=response),
+             #   width = 0, size = 0.7) +
+  labs(x = '',
+       # y= expression(paste('Effect of NPK on Species Richness'))
+       y='')+
+  geom_vline(xintercept = 0, lty = 2) +
+ # scale_y_continuous(breaks=c(-100,50)) +
+  #scale_color_manual(values = c("#000000","#0B775E")) +
+ # coord_flip()+
+  labs(title= "Species Loss")+ 
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = -0.2, unit = "cm"),
+                               #axis.title.y = element_blank(),
+                              # axis.title.x = element_blank(),
+                               axis.text.y = element_text(size=8),
+                               axis.text.x = element_text(size=8),
+                               title=element_text(size=8),
+                               strip.background = element_blank(),legend.position="none")
+
+sloss.se
+
+sl.se<-ggplot() + 
+  # facet_grid(~model)+
+  geom_point(data =npk.effs, aes(y = Quadrant, x = sl.trt.rate.p),
+             colour= "#B40F20",size = 2, position = position_jitter(height = 0.15 )) +
+  # geom_errorbar(data = rich.p, aes(x = response, ymin = eff_lower,
+  #                                  ymax = eff_upper, color=response),
+  #   width = 0, size = 0.7) +
+  labs(x = '',
+       # y= expression(paste('Effect of NPK on Species Richness'))
+       y='')+
+  geom_vline(xintercept = 0, lty = 2) +
+  # scale_y_continuous(breaks=c(-100,50)) +
+  #scale_color_manual(values = c("#000000","#0B775E")) +
+ # coord_flip()+
+  labs(title= "Species Loss: Biomass")+ 
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = -0.2, unit = "cm"),
+                               axis.title.y = element_blank(),
+                               axis.text.y = element_blank(),
+                               #axis.text.y = element_text(size=6),
+                               axis.text.x = element_text(size=8),
+                               title=element_text(size=8),
+                               strip.background = element_blank(),legend.position="none")
+
+sl.se
+
+
+sgain.se<-ggplot() + 
+  # facet_grid(~model)+
+  geom_point(data =npk.effs, aes(y = Quadrant, x = sgain.trt.rate.p),
+             colour= "#046C9A",size = 2, position = position_jitter(height = 0.15 )) +
+  # geom_errorbar(data = rich.p, aes(x = response, ymin = eff_lower,
+  #                                  ymax = eff_upper, color=response),
+  #   width = 0, size = 0.7) +
+  labs(x = '',
+       # y= expression(paste('Effect of NPK on Species Richness'))
+       y='')+
+  geom_vline(xintercept = 0, lty = 2) +
+  # scale_y_continuous(breaks=c(-100,50)) +
+  #scale_color_manual(values = c("#000000","#0B775E")) +
+  #coord_flip()+
+  labs(title= "Species Gain", y= 'Study-Level Slope')+ 
+  theme_bw()+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               #plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = -0.2, unit = "cm"),
+                               axis.title.y = element_blank(),
+                               axis.text.y = element_blank(),
+                               axis.title.x = element_text(size=8),
+                               axis.text.x = element_text(size=8),
+                               title=element_text(size=8),
+                               strip.background = element_blank(),legend.position="none")
+
+sgain.se
+
+sg.se<-ggplot() + 
+  # facet_grid(~model)+
+  geom_point(data =npk.effs, aes(y = Quadrant, x = sg.trt.rate.p),
+             colour= "#046C9A",size = 2, position = position_jitter(height = 0.15 )) +
+  # geom_errorbar(data = rich.p, aes(x = response, ymin = eff_lower,
+  #                                  ymax = eff_upper, color=response),
+  #   width = 0, size = 0.7) +
+  labs(x = '',
+       # y= expression(paste('Effect of NPK on Species Richness'))
+       y='')+
+  geom_vline(xintercept = 0, lty = 2) +
+  # scale_y_continuous(breaks=c(-100,50)) +
+  #scale_color_manual(values = c("#000000","#0B775E")) +
+ # coord_flip()+
+  labs(title= "Species Gain: Biomass")+ 
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = -0.2, unit = "cm"),
+                               axis.title.y = element_blank(),
+                               axis.text.y = element_blank(),
+                               axis.text.x = element_text(size=8),
+                               title=element_text(size=8),
+                               strip.background = element_blank(),legend.position="none")
+
+sg.se
+
+
+cde.se<-ggplot() + 
+  # facet_grid(~model)+
+  geom_point(data =npk.effs, aes(y = Quadrant, x = cde.trt.rate.p),
+             colour= "#F98400",size = 2, position = position_jitter(height = 0.15 )) +
+  # geom_errorbar(data = rich.p, aes(x = response, ymin = eff_lower,
+  #                                  ymax = eff_upper, color=response),
+  #   width = 0, size = 0.7) +
+  labs(x = '',
+       # y= expression(paste('Effect of NPK on Species Richness'))
+       y='')+
+  geom_vline(xintercept = 0, lty = 2) +
+ scale_x_continuous(breaks=c(-100,-50,50)) +
+  #scale_color_manual(values = c("#000000","#0B775E")) +
+  #coord_flip()+
+  labs(title= "Persistent Species") + 
+  geom_text(data = npk.effs %>%
+              group_by(Quadrant) %>%
+              mutate(n_study = n_distinct(site_code)) %>%
+              ungroup() %>%
+              distinct(Quadrant, n_study, .keep_all = T),
+            aes(x=-45, y=Quadrant,
+                label=paste('n[study] == ', n_study)),
+            size=3.5,
+            nudge_y =0.20, parse = T) +
+  theme_bw(base_size=14)+theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                               plot.margin= margin(t = 0.2, r = 0.2, b = -0.2, l = -0.2, unit = "cm"),
+                               axis.title.y = element_blank(),
+                               axis.text.y = element_blank(),
+                               axis.text.x = element_text(size=8),
+                               title=element_text(size=8),
+                               strip.background = element_blank(),legend.position="none")
+
+
+cde.se
+
+(sloss.se | sl.se | sgain.se | sg.se | cde.se)
