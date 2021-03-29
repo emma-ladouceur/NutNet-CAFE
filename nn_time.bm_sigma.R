b05671976db8ed3f1439300f8b5062284892c094
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-03-29 10:53:45 +0200

    bm sigma

--- nn_time.bm_sigma.R
+++ nn_time.bm_sigma.R
@@ -14,8 +14,8 @@ plot$plot<-as.factor(plot$plot)
 plot <- plot %>% group_by(site_code) %>% filter(max.year >= 3) %>%
   ungroup()
 
-plot.bm.3_sigma <- brm( bf( plot.mass ~ trt * year_trt + (trt * year_trt | site_code/block/plot) + trt.y:year.y.m, 
-                            sigma ~ 0 + trt.y + (0 + trt.y | site_code) ), 
+plot.bm.3_sigma <- brm( bf( plot.mass ~ trt * year_trt + (trt * year_trt | site_code/block/plot) + trt:year_trt, 
+                            sigma ~ 0 + trt + (0 + trt | site_code) ), 
                   data = plot , family=student(),  cores = 4,iter=6000, warmup = 1000, chains = 4)
 
 
