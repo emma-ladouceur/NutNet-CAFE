68c2612312b559f63296c95504112aa6ab70f32d
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-04-21 08:32:55 +0200

    sigma model test

--- cluster/univariate_models/nn_time.sl_sigma.R
+++ cluster/univariate_models/nn_time.sl_sigma.R
@@ -15,10 +15,16 @@ p.all$plot<-as.factor(p.all$plot)
 p.all <- p.all %>% group_by(site_code) %>% filter(max.year >= 3) %>%
   ungroup()
 
-sl.3_sigma <- brm( bf( SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot) + trt.y:year.y.m, 
-            sigma ~ 0 + trt.y + (0 + trt.y | site_code) ),
-                 data = p.all, family=student(), cores = 4, iter=15000, warmup = 1000, chains = 4,
-            control = list(adapt_delta = 0.99) )
+# sl.3_sigma <- brm( bf( SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot) + trt.y:year.y.m,
+#             sigma ~ 0 + trt.y + (0 + trt.y | site_code) ),
+#                  data = p.all, family=student(), cores = 4, iter=15000, warmup = 1000, chains = 4,
+#             control = list(adapt_delta = 0.99) )
+
+
+sl.3_sigma <- brm( bf( SL ~  trt.y * year.y.m + (trt.y + year.y.m |  site_code/block/plot) + trt.y:year.y.m,
+                       sigma ~ 0 + trt.y + (0 + trt.y | site_code) ),
+                   data = p.all, family=student(), cores = 4, iter=15000, warmup = 1000, chains = 4,
+                   control = list(adapt_delta = 0.99) )
 
 
 save(sl.3_sigma,
