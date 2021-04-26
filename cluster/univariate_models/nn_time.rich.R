5055152f79b0ef4eae7d960ce9d45f4e9132816a
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-04-26 17:43:13 +0200

    rich model

--- cluster/univariate_models/nn_time.rich.R
+++ cluster/univariate_models/nn_time.rich.R
@@ -10,13 +10,13 @@ plot <- read.csv(paste0(path, '/plot.csv'), header=T,fill=TRUE,sep=",",na.string
 plot$site_code<-as.factor(plot$site_code)
 plot$block<-as.factor(plot$block)
 plot$plot<-as.factor(plot$plot)
-plot$log.rich<-log(plot$all.div)
+
 
  plot <- plot %>% group_by(site_code) %>% filter(year_max >= 3) %>%
  ungroup()
 
 
-plot.rich.3 <- brm(all.div ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
+plot.rich.3 <- brm(rich ~  trt * year_trt + (trt * year_trt | site_code/block/plot), 
                     data = plot,cores = 4,iter=6000, warmup = 1000, chains = 4)
 
 save(plot.rich.3,
