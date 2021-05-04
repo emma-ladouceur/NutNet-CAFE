fab0a8911ea9d792580b990bb701876665c2fa45
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-04 12:48:53 +0200

    new submit sbatch

--- cluster/price_pairs/Price_Pairs_Submit.sh
+++ cluster/price_pairs/Price_Pairs_Submit.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-#$ -S /bin/bash
-#$ -N Price_Pairs
+#SBATCH -J Price_Pairs
 
-#$ -l h_rt=24:00:00
-#$ -l h_vmem=8G
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=8G
 
-#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
-#$ -j y
+# %u is the UserName
+# %x is the JobName
+# %j is the JobID
+# Note: These %patterns ONLY work for specifying --output/-o and --error/-e
+# If -e is not specified, stdout and stderr will automatically be merged together
+#SBATCH -o /work/%u/%x-%j.log
 
-#$ -binding linear:1
+# Only send E-Mail when the job has ended
+#SBATCH --mail-type=END
+#SBATCH --mail-user=emma.ladouceur@ufz.de
 
-#$ -m a
-#$ -M emma.ladouceur@ufz.de
-
-#$ -cwd
+# Current Working directory is automatically set to the directory the job is submitted from
+# Can be changed with the --chdir option, e.g:
+# #SBATCH --chdir /work/ladouceur/...
+# Note that $USER variable can't be used in any lines that are prefixed with #SBATCH
 
 module load foss/2019b R/3.6.2-2
 
 experiment=$1
 input=$2
-output_dir=/work/$USER/$JOB_NAME/$experiment
+output_dir=/work/$USER/$SLURM_JOB_NAME/$experiment
 mkdir -p "$output_dir"
 
 prefix=$(basename "$input" .rds)
@@ -29,3 +34,5 @@ Rscript \
   Price_Pairs.R \
   "$input" \
   "$output_dir/$prefix.rds"
+  
+  
