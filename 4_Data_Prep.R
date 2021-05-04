cfc30c16e43c55005a10d982f4b7bbdead84de16
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-04 14:41:39 +0200

    figure 4

--- 4_Data_Prep.R
+++ 4_Data_Prep.R
@@ -21,9 +21,9 @@ library(foreach)
 
 
 # plot data
-p <- read.csv("~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/new/plot.csv",header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
+p <- read.csv("~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/plot.csv",header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 # run cluster -based price script, then load the new input
-price.list <- list.files(path = "~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/new/price pairs data/", pattern = ".rds$", recursive = TRUE, full.names = TRUE)
+price.list <- list.files(path = "~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/price pairs data/", pattern = ".rds$", recursive = TRUE, full.names = TRUE)
 
 # bring in all 428 pairwise price datasets
 price.all <- foreach (file = price.list,.combine=rbind) %do% {
@@ -34,18 +34,21 @@ price.all <- foreach (file = price.list,.combine=rbind) %do% {
 head(price.all)
 
 # we keep this unpruned version just in case we need to check or change something
-write.csv(price.all,"~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/new/nutnet_price_all.csv")
+write.csv(price.all,"~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/nutnet_price_all.csv")
 
 # load unpruned data frame
-price.all <- read.csv("~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/new/nutnet_price_all.csv",header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
+price.all <- read.csv("~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/nutnet_price_all.csv",header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 
 # the function compares everything possible so we now need to prune the data back to only meaningful comparisons
 head(price.all)
 nrow(price.all)
 # 94,828 rows, most of them unnecessary and meaningless
 
+
 # separate and unite the columns to improve ease of sorting, filtering and pruning the dataset
-price.sort <- price.all %>% separate(trt_year,into=c("trt_year.x","trt_year.y"),sep = " ", remove=FALSE) %>%
+price.sort <- price.all %>% 
+  mutate( site.year.id = str_replace( site.year.id, "tejon_", "tejon"),) %>% # reformat tejon_foot.us  and tejon_south.us to match other site code formats
+  separate(trt_year,into=c("trt_year.x","trt_year.y"),sep = " ", remove=FALSE) %>%
   # separate columns for filtering and analysis
   separate(trt_year.y, into=c("trt.y","year.y"),sep = "_", remove=FALSE) %>%
   separate(trt_year.x, into=c("trt.x","year.x"),sep = "_", remove=FALSE) %>%
@@ -59,17 +62,22 @@ price.sort <- price.all %>% separate(trt_year,into=c("trt_year.x","trt_year.y"),
   unite("unique.id", c("site.year.id","trt_year","block","plot"), sep="_", remove=FALSE)
 
 # have a look at all the levels that we need to filter
-levels(price.all$trt.xy)
-levels(price.all$block)
-levels(price.all$year.xy)
-levels(price.all$plot)
+price.sort$trt.xy<- as.factor(as.character(price.sort$trt.xy))
+levels(price.sort$trt.xy)
+price.sort$block<- as.factor(as.character(price.sort$block))
+levels(price.sort$block)
+price.sort$year.xy<- as.factor(as.character(price.sort$year.xy))
+levels(price.sort$year.xy)
+price.sort$plot<- as.factor(as.character(price.sort$plot))
+levels(price.sort$plot)
+
 
 # filter data down to only the comparisons we want
 price.pairs <-  price.sort  %>%
   # filter for meaningful comparisons
   filter(trt.xy %in% c('Control_Control','NPK_NPK'), # filter treatment comparisons
          block %in% c('1 1','2 2','3 3','4 4','5 5','6 6'), # filter within block comparisons
-         year.xy %in% c('0_1','0_2','0_3','0_4','0_5','0_6','0_7','0_8','0_9','0_10','0_11','0_12'), # filter year comparisons
+         year.xy %in% c('0_1','0_2','0_3','0_4','0_5','0_6','0_7','0_8','0_9','0_10','0_11','0_12', '0_13'), # filter year comparisons
          plot %in% c('1 1','2 2','3 3','4 4','5 5','6 6','7 7','8 8','9 9','10 10','11 11','12 12','13 13','14 14','15 15', # filter plot comparisons
                      '16 16','17 17','18 18','19 19','20 20','21 21','22 22','23 23','24 24','25 25','26 26','27 27','28 28',
                      '29 29','30 30','31 31','32 32','33 33','34 34','35 35','36 36','37 37', '38 38','39 39','40 40','41 41',
@@ -77,6 +85,8 @@ price.pairs <-  price.sort  %>%
                      '56 56','57 57','58 58','59 59','60 60','61 61','62 62')) 
 
 
+price.pairs$plot<- as.factor(as.character(price.pairs$plot))
+levels(price.pairs$plot)
 # calculate some of the extra metrics we need for modeling
 
 price.pairs$year.y <- as.numeric(price.pairs$year.y)
@@ -100,11 +110,13 @@ price.pairs.calc$plot.x <- as.factor(as.character(price.pairs.calc$plot.x))
 price.pairs.calc$year.y <- as.numeric(price.pairs.calc$year.y)
 price.pairs.calc$year_max <- as.numeric(price.pairs.calc$year_max)
 
+
+
 # look at what sites will be included in main analysis
 sites <- price.pairs.calc %>% distinct(site_code, year_max) %>% filter(year_max >= 3)
 
 View(sites) # looks good!!
 
-write.csv(price.pairs.calc,"~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/new/nutnet_cumulative_time.csv")
+write.csv(price.pairs.calc,"~/GRP GAZP Dropbox/Emma Ladouceur/_Projects/NutNet/Data/nutnet_cumulative_time.csv")
 
 
--- cluster/univariate_models/nn_time.rich.sh
+++ cluster/univariate_models/nn_time.rich.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#$ -N nn_time.rich
-#$ -S /bin/bash
-#$ -l h_rt=24:00:00
-#$ -l h_vmem=8G
-#$ -pe smp 4
+#SBATCH -J nn_time.rich
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=4
 
-#$ -o /home/$USER/$JOB_NAME-$JOB_ID.out
-#$ -j y
+# Output files should ideally go to /work instead of /home
+#SBATCH -o /work/%u/%x-%j.out
 
 module load foss/2019b R/3.6.2-2
-export OFILE=/work/$USER/NutNet/$JOB_NAME-$JOB_ID.Rdata
+export OFILE=/work/$USER/NutNet/$SLURM_JOB_NAME-$SLURM_JOB_ID.Rdata
 export LANG=en_US.UTF-8
 Rscript --vanilla /home/ladouceu/projects/NutNet-CAFE/cluster/univariate_models/nn_time.rich.R
-
-
