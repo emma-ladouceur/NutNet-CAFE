119b1ac7a4f4472cf9283350311fdef65067a192
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-06 09:53:52 +0200

    idk

--- cluster/univariate_models/rich.bm.sh
+++ cluster/univariate_models/rich.bm.sh
@@ -8,8 +8,8 @@
 # Output files should ideally go to /work instead of /home
 #SBATCH -o /work/%u/%x-%j.out
 
-lines=$(wc -l rich.bm.parameters.csv)
-sbatch -a 2-$lines rich.bm.R
+lines=$(wc -l /gpfs1/data/idiv_chase/emmala/NutNet/rich.bm.parameters.csv)
+sbatch -a 2-$lines /home/ladouceu/projects/NutNet-CAFE/cluster/univariate_models/rich.bm.R
 
 module load foss/2019b R/3.6.2-2
 export OFILE=/work/$USER/NutNet/$SLURM_JOB_NAME-$SLURM_JOB_ID.Rdata
