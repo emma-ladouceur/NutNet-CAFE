d672e32de357fee0d813045ee4aa03ac408fefb3
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-08 14:23:07 +0200

    sg ps sigma i

--- cluster/univariate_models/sg_sigma.R
+++ cluster/univariate_models/sg_sigma.R
@@ -15,12 +15,12 @@ p.all$plot<-as.factor(p.all$plot)
 p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 3) %>%
   ungroup()
 
-sg.3_sigma <- brm(bf( SG ~  trt.y + year.y.m + (trt.y * year.y.m |  site_code/block/plot) + trt.y:year.y.m,
+sg.3_sigmai <- brm(bf( SG ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot) + trt.y:year.y.m,
                    sigma ~ 0 + trt.y + (0 + trt.y | site_code) ),
                 data = p.all, family=student(),cores = 4,iter=20000, warmup = 1000, chains = 4, 
               control = list(adapt_delta = 0.99) )
 
 
-save(sg.3_sigma,
+save(sg.3_sigmai,
      file=Sys.getenv('OFILE'))
 
