944bb15fd644726fcd3345b1e6cd0894a940bf43
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-22 16:45:44 +0200

    cover

---
+++ cluster/univariate_models/cover.R
@@ -0,0 +1,23 @@
+
+library(tidyverse)
+library(brms)
+
+
+path <- '/gpfs1/data/idiv_chase/emmala/NutNet'
+plot <- read.csv(paste0(path, '/plot.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
+
+
+plot$site_code<-as.factor(plot$site_code)
+plot$block<-as.factor(plot$block)
+plot$plot<-as.factor(plot$plot)
+
+plot <- plot %>% group_by(site_code) %>% filter(year_max >= 3) %>%
+  ungroup()
+
+cover.3 <- brm( plot.cover ~ trt * year_trt + (trt * year_trt | site_code/block/plot), 
+                  data = plot , family=student(),  cores = 4,iter=6000, warmup = 1000, chains = 4)
+
+
+save(cover.3,
+     file=Sys.getenv('OFILE'))
+
