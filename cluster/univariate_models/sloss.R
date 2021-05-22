e0ae6dc4feb962bc1ab24d7f715f6b0d5cd48328
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-05-22 14:55:11 +0200

    year 6

--- cluster/univariate_models/sloss.R
+++ cluster/univariate_models/sloss.R
@@ -12,14 +12,14 @@ p.all$block<-as.factor(p.all$block)
 p.all$plot<-as.factor(p.all$plot)
 
 
-p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 3) %>%
+p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 6) %>%
   ungroup()
 
- sloss.3 <- brm(s.loss.n ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+ sloss.6 <- brm(s.loss.n ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
                  data = p.all,family=student(), cores = 4, iter = 15000, warmup = 1000,chains = 4,
                  control = list(adapt_delta = 0.99))
 
-save(sloss.3,
+save(sloss.6,
      file=Sys.getenv('OFILE'))
 
 
