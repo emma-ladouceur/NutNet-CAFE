1f69357d875b607ec81f60cf81b208623f0ec14e
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-07-14 16:56:23 +0200

    sl

--- cluster/univariate_models/sl.R
+++ cluster/univariate_models/sl.R
@@ -13,15 +13,15 @@ p.all$site.year.id<-as.factor(p.all$site.year.id)
 p.all$block<-as.factor(p.all$block)
 p.all$plot<-as.factor(p.all$plot)
 
-p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 3) %>%
+p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 6) %>%
   ungroup()
 
-sl.3 <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+sl.6<- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
                  data = p.all, family=student(), cores = 4, iter=20000,warmup = 1000, chains = 4,
             control = list(adapt_delta = 0.99) )
 
 
-save(sl.3,
+save(sl.6,
      file=Sys.getenv('OFILE'))
 
 
