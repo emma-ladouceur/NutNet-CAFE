33944a008c9309ae665448aea4523a7b79d0378a
Emma Ladouceur <emma.ladouceur@idiv.de>
2021-07-16 06:56:37 +0200

    sl

--- cluster/univariate_models/sl.R
+++ cluster/univariate_models/sl.R
@@ -4,8 +4,8 @@ library(brms)
 
 
 path <- '/gpfs1/data/idiv_chase/emmala/NutNet'
-p.all <- read.csv(paste0(path, '/nutnet_cumulative_time.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
-#p.all <- read.csv(paste0(path, '/nutnet_cumulative_time_cover.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
+#p.all <- read.csv(paste0(path, '/nutnet_cumulative_time.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
+p.all <- read.csv(paste0(path, '/nutnet_cumulative_time_cover.csv'), header=T,fill=TRUE,sep=",",na.strings=c(""," ","NA","NA ","na"))
 
 
 p.all$site_code<-as.factor(p.all$site_code)
@@ -16,12 +16,12 @@ p.all$plot<-as.factor(p.all$plot)
 p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 3) %>%
   ungroup()
 
-sl.3 <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+sl.3.cover <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
                  data = p.all, family=student(), cores = 4, iter=25000, warmup = 1000, chains = 4,
             control = list(adapt_delta = 0.99) )
 
 
-save(sl.3,
+save(sl.3.cover,
      file=Sys.getenv('OFILE'))
 
 
