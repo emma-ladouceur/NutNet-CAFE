b7df61c803a87c2ae4f140e17781138f266a11fc
Emma Rachel Ladouceur <el50nico@idivmac57.local>
2021-07-19 13:49:57 +0200

    some stuffs

--- cluster/univariate_models/sl.sh
+++ cluster/univariate_models/sl.sh
@@ -4,6 +4,7 @@
 #SBATCH --time=24:00:00
 #SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=4
+#SBATCH --mail-type=BEGIN,END
 
 # Output files should ideally go to /work instead of /home
 #SBATCH -o /work/%u/%x-%j.out
