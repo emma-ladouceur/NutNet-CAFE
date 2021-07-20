4e33cc395617befe49d51a9c8ecbfa8af3eede99
Emma Rachel Ladouceur <el50nico@idivmac57.local>
2021-07-20 23:12:31 +0200

    sl try again

--- cluster/univariate_models/sl.R
+++ cluster/univariate_models/sl.R
@@ -31,8 +31,8 @@ sl.3_p <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot
                          prior(normal(0,10), class = sd),
                          prior(normal(0,10), class = sigma),
                          prior(constant(10), class = nu)),
-                       control = list(max_treedepth = 12),
-              control = list(adapt_delta = 0.99) )
+                       control = list(max_treedepth = 12,
+                                      adapt_delta = 0.99) )
 
 
 
