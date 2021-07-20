147b27a5817b140440e84869b5305fddd7ef0dcd
Emma Rachel Ladouceur <el50nico@idivmac57.local>
2021-07-20 22:13:03 +0200

    sl prior test

--- cluster/univariate_models/sl.R
+++ cluster/univariate_models/sl.R
@@ -16,12 +16,26 @@ p.all$plot<-as.factor(p.all$plot)
 p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 3) %>%
   ungroup()
 
-sl.3_test2 <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
-                 data = p.all, family=student(), cores = 4, chains = 4,
-                 control = list(max_treedepth = 12) )
+# sl.3_test2 <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+#                  data = p.all, family=student(), cores = 4, chains = 4,
+#                  control = list(max_treedepth = 12) )
 
 
-save(sl.3_test2,
+sl.3_p <- brm(SL ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+                       data = p.all, family=student(), cores = 4, chains = 4,
+                       prior = c(
+                         prior(normal(-22,30), class = Intercept),
+                         prior(normal(-35,10), class = b, coef = trt.yNPK),
+                         prior(normal(0,10), class = b, coef = year.y.m),
+                         prior(normal(0,10), class = b, coef = trt.yNPK:year.y.m),
+                         prior(normal(0,10), class = sd),
+                         prior(normal(0,10), class = sigma),
+                         prior(constant(10), class = nu)),
+                       control = list(max_treedepth = 12))
+
+
+
+save(sl.3_p,
      file=Sys.getenv('OFILE'))
 
 
