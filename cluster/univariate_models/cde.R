4980077d0e349975092434b626738b5d12569cec
Emma Ladouceur <emmala@gmail.com>
2021-09-08 11:12:47 +0200

    cde p

--- cluster/univariate_models/cde.R
+++ cluster/univariate_models/cde.R
@@ -22,18 +22,35 @@ p.all <- p.all %>% group_by(site_code) %>% filter(year_max >= 3) %>%
 #              control = list(adapt_delta = 0.99))
 
 
+# cde.3_p <- brm(CDE ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
+#               data = p.all, family=student(), cores = 4, chains = 4,
+#              # iter = 5000, warmup = 1000,
+#               prior = c(
+#                 prior(normal(41,160), class = Intercept),
+#                 prior(normal(160,10), class = b, coef = trt.yNPK),
+#                 prior(normal(0,10), class = b, coef = year.y.m),
+#                 prior(normal(0,10), class = b, coef = trt.yNPK:year.y.m),
+#                 prior(normal(0,10), class = sd),
+#                 prior(normal(0,10), class = sigma),
+#                 prior(constant(10), class = nu)),
+#               control = list(max_treedepth = 12))
+
+
 cde.3_p <- brm(CDE ~  trt.y * year.y.m + (trt.y * year.y.m |  site_code/block/plot), 
-              data = p.all, family=student(), cores = 4, chains = 4,
-             # iter = 5000, warmup = 1000,
-              prior = c(
-                prior(normal(41,160), class = Intercept),
-                prior(normal(160,10), class = b, coef = trt.yNPK),
-                prior(normal(0,10), class = b, coef = year.y.m),
-                prior(normal(0,10), class = b, coef = trt.yNPK:year.y.m),
-                prior(normal(0,10), class = sd),
-                prior(normal(0,10), class = sigma),
-                prior(constant(10), class = nu)),
-              control = list(max_treedepth = 12))
+               data = p.all, family=student(), cores = 4, chains = 4,
+               iter = 5000, warmup = 1000,
+               prior = c(
+                 prior(normal(41,160), class = Intercept),
+                 prior(normal(160,10), class = b, coef = trt.yNPK),
+                 prior(normal(0,10), class = b, coef = year.y.m),
+                 prior(normal(0,10), class = b, coef = trt.yNPK:year.y.m),
+                 prior(normal(0,10), class = sd),
+                 prior(normal(0,10), class = sigma),
+                 prior(constant(10), class = nu)),
+               control = list(adapt_delta = 0.99)#,
+               #sample_prior = 'only',
+              # backend = 'cmdstanr'
+)
 
 
 save(cde.3_p,
