6bae0f4b91bf050d1554dcd37dc7bb8d4b8ea783
Emma Ladouceur <emmala@gmail.com>
2021-12-07 11:11:52 +0100

    for magda

--- cluster/univariate_models/rich.R
+++ cluster/univariate_models/rich.R
@@ -12,10 +12,10 @@ plot$block<-as.factor(plot$block)
 plot$plot<-as.factor(plot$plot)
 
 
- plot <- plot %>% group_by(site_code) %>% filter(year_max >= 10) %>%
+ plot <- plot %>% group_by(site_code) %>% filter(year_max >= 3) %>%
  ungroup()
  
- rich.10 <- brm(rich ~ trt * year_trt + (trt * year_trt | site_code/block/plot),
+ rich.3 <- brm(rich ~ trt * year_trt + (trt * year_trt | site_code/block/plot),
                  data = plot, cores = 4, chains = 4,
                  iter=5000, warmup = 1000,
                  prior = c(
@@ -25,12 +25,10 @@ plot$plot<-as.factor(plot$plot)
                    prior(normal(0,1), class = b, coef = trtNPK:year_trt),
                    prior(normal(0,1), class = sd),
                    prior(normal(0,1), class = sigma)),
-                 control = list(adapt_delta = 0.99)#,
-                 #sample_prior = 'only',
-                 #backend = 'cmdstanr'
+                 control = list(adapt_delta = 0.99)
  )
 
-save(rich.10,
+save(rich.3,
      file=Sys.getenv('OFILE'))
 
 
